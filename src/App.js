fb19958e9e94c3ddead12545c5e755b4d0819804
happai079 <gksyddl22@gmail.com>
2022-11-01 23:39:35 +0900

    style : app container 기본 width/height 지정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Home from './pages/Home';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+	margin: 0 auto;
+	max-width: 393px;
+	min-height: 852px;
+	box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+`;
 
 function App() {
 	return (
-		<div className="App">
+		<AppContainer className="App">
 			<Home />
-		</div>
+		</AppContainer>
 	);
 }
 
