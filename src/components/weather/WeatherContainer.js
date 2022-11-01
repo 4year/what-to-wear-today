77e9a445ede129aaec1ee7452aad8676f2329c1d
huunguk <huunguk@gmail.com>
2022-11-01 21:51:56 +0900

    # feat : 새로운 기능 추가

--- src/components/weather/WeatherContainer.js
+++ src/components/weather/WeatherContainer.js
@@ -1,8 +1,72 @@
 // weather container
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 393px;
+  border: 3px solid black;
+  font-size: 14px;
+  line-height: 30px;
+  font-weight: 800;
+
+  .temp {
+    color: navy;
+  }
+`;
 
 const WeatherContainer = () => {
-	return <div></div>;
+  const dateBuilder = (d) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${date} ${month} ${year}`;
+  };
+
+  const Day = () => {
+    const week = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let now = new Date();
+    let dayOfWeek = week[now.getDay()];
+    return `${dayOfWeek}`;
+  };
+
+  return (
+    <Container>
+      <div className="location-box">
+        <div className="location">Republic of Korea</div>
+        <div className="date">{dateBuilder(new Date())}</div>
+      </div>
+      {/* <img src="" alt="sunny">  이미지 첨부 예정 */}
+      <div className="weather-box">
+        <div className="temp">15°C</div>
+        {/* 기온은 날씨api 받아서 사용  */}
+        <div className="weather">{Day(new Date())}</div>
+      </div>
+    </Container>
+  );
 };
 
 export default WeatherContainer;
