96e52ac804e891f3a10332a76022e5a5d1e2816f
happai079 <gksyddl22@gmail.com>
2022-11-03 13:30:33 +0900

    style : width 변경

--- src/pages/Loadging.js
+++ src/pages/Loadging.js
@@ -9,8 +9,8 @@ const LoadgingContainer = styled.div`
 	flex-direction: column;
 	align-content: center;
 	justify-content: center;
-	width: 100vw;
-	height: 100vh;
+	width: 100%;
+	height: 100%;
 `;
 
 const Loadging = () => {
