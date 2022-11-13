fb1face04982f1bea017fa31aea18590492c2849
happai079 <gksyddl22@gmail.com>
2022-11-13 23:55:57 +0900

    style : 배경 물결 모양 SVG 추가

--- src/components/weather/WeatherContainer.js
+++ src/components/weather/WeatherContainer.js
@@ -69,21 +69,35 @@ const WeatherContainer = ({ weather }) => {
 
 	return (
 		<Container background={background}>
-			<CurrentWeather>
-				<div className="location-box">
-					<div className="location">{weather.name}</div>
-					<div className="date">{dateBuilder(new Date())}</div>
-				</div>
-				<img
-					src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-					alt="weather icon"
-				/>
-				<p>{weather.weather[0].description}</p>
-				<div className="weather-box">
-					<div className="temp">{temperature}°C</div>
-					<div className="weather">{Day(new Date())}</div>
-				</div>
-			</CurrentWeather>
+			<div className="wave">
+				<svg
+					data-name="Layer 1"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 1200 120"
+					preserveAspectRatio="none"
+				>
+					<path
+						d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+						fill="#FFFFFF"
+						fill-opacity="1"
+					></path>
+				</svg>
+				<CurrentWeather>
+					<div className="location-box">
+						<div className="location">{weather.name}</div>
+						<div className="date">{dateBuilder(new Date())}</div>
+					</div>
+					<img
+						src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+						alt="weather icon"
+					/>
+					<p>{weather.weather[0].description}</p>
+					<div className="weather-box">
+						<div className="temp">{temperature}°C</div>
+						<div className="weather">{Day(new Date())}</div>
+					</div>
+				</CurrentWeather>
+			</div>
 			<WeeklyWeather />
 		</Container>
 	);
