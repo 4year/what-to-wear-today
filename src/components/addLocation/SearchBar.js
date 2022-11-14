03510e82f7d62b9c0b6c2bdf7f1f4a097ebf5408
taeyeess <taeyeeya@gmail.com>
2022-11-14 19:00:16 +0900

    style : searchBar 퍼블리싱

--- src/components/addLocation/SearchBar.js
+++ src/components/addLocation/SearchBar.js
@@ -1,8 +1,38 @@
 // 주소 검색창
 import React from "react";
+import styled from "styled-components";
+import { IoIosArrowBack } from 'react-icons/io';
 
-const SearchBar = () => {
-  return <div></div>;
+
+const SearchBar = ({ show }) => {
+  return (
+    <div>
+     {show && (
+      <SearchLocation>
+        {/* <IoIosArrowBack/> */}
+        <input 
+          className="search-input" 
+          type="text" 
+          placeholder="내 동네 이름 (동,읍,면)으로 검색">
+        </input>
+      </SearchLocation>
+     )}
+    </div>
+  );
 };
+
+const SearchLocation = styled.header`  
+  display: flex;
+  align-items: center;  
+  .search-input{
+    width: 320px;
+    padding: 8px 14px;
+    border: 0;
+    border-radius: 20px;
+    background-color: #F2F2F2;
+    margin-bottom: 10px;    
+  }
+
+`
 
 export default SearchBar;
