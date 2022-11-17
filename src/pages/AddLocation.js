7446caec8e27f993b00e7e4ec60dbf2e8de4bd01
taeyeess <taeyeeya@gmail.com>
2022-11-17 22:31:55 +0900

    style: 오타수정

--- src/pages/AddLocation.js
+++ src/pages/AddLocation.js
@@ -1,8 +1,8 @@
 // 주소 찾기 페이지 - 검색창, 현재 위치 찾기 버튼, 검색결과
 import React from 'react';
 
-const AddLoction = () => {
+const AddLocation = () => {
 	return <div></div>;
 };
 
-export default AddLoction;
+export default AddLocation;
