193cbdc8f35766b040189222521cf8724e209814
huunguk <huunguk@gmail.com>
2022-11-20 16:29:59 +0900

    style: weekly weather api 적용 완료

--- src/components/weather/WeeklyWeather.js
+++ src/components/weather/WeeklyWeather.js
@@ -5,164 +5,184 @@ import { TbTemperature } from 'react-icons/tb';
 import { TiWeatherSunny } from 'react-icons/ti';
 import { TiWeatherShower } from 'react-icons/ti';
 
-const WeeklyWeather = () => {
-	return (
-		<>
-			{/* 내일 */}
-			<Container>
-				<Title>
-					<h3>내일</h3>
-					<div>
-						<TbTemperature size="20px" />
-						<TiWeatherSunny size="20px" />
-						<TiWeatherShower size="20px" />
-					</div>
-				</Title>
-				<Wrap>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>0시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>3시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>6시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>9시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>12시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>15시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>18시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>21시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>24시</div>
-					</WrapList>
-				</Wrap>
-			</Container>
-			{/* 모레 */}
-			<Container>
-				<Title>
-					<h3>모레</h3>
-					<div>
-						<TbTemperature size="20px" />
-						<TiWeatherSunny size="20px" />
-						<TiWeatherShower size="20px" />
-					</div>
-				</Title>
-				<Wrap>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>0시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>3시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>6시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>9시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>12시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>15시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>18시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>21시</div>
-					</WrapList>
-					<WrapList>
-						<div>{}°C</div>
-						<div>img</div> {/* <img src="" alt="" /> */}
-						<div>24시</div>
-					</WrapList>
-				</Wrap>
-			</Container>
-		</>
-	);
+const WeeklyWeather = ({ weekly }) => {
+  //내일
+  const temperature0 = Math.round(weekly.list[5].main.temp);
+  const temperature3 = Math.round(weekly.list[6].main.temp);
+  const temperature6 = Math.round(weekly.list[7].main.temp);
+  const temperature9 = Math.round(weekly.list[8].main.temp);
+  //   const temperature12 = Math.round(weekly.list[9].main.temp);
+  //   const temperature15 = Math.round(weekly.list[10].main.temp);
+  //   const temperature18 = Math.round(weekly.list[11].main.temp);
+  //   const temperature21 = Math.round(weekly.list[12].main.temp);
+  //   const temperature24 = Math.round(weekly.list[13].main.temp);
+  //모레
+  //   const Twotemperature0 = Math.round(weekly.list[14].main.temp);
+  //   const Twotemperature3 = Math.round(weekly.list[15].main.temp);
+  //   const Twotemperature6 = Math.round(weekly.list[16].main.temp);
+  //   const Twotemperature9 = Math.round(weekly.list[17].main.temp);
+  //   const Twotemperature12 = Math.round(weekly.list[18].main.temp);
+  //   const Twotemperature15 = Math.round(weekly.list[19].main.temp);
+  //   const Twotemperature18 = Math.round(weekly.list[20].main.temp);
+  //   const Twotemperature21 = Math.round(weekly.list[21].main.temp);
+  //   const Twotemperature24 = Math.round(weekly.list[22].main.temp);
+
+  return (
+    <>
+      {/* 내일 */}
+      <Container>
+        <Title>
+          <h3>내일</h3>
+          <div>
+            <TbTemperature size="20px" />
+            <TiWeatherSunny size="20px" />
+            <TiWeatherShower size="20px" />
+          </div>
+        </Title>
+        <Wrap>
+          <WrapList>
+            <div>{temperature0}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>0시</div>
+          </WrapList>
+          <WrapList>
+            <div>{temperature3}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>3시</div>
+          </WrapList>
+          <WrapList>
+            <div>{temperature6}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>6시</div>
+          </WrapList>
+          <WrapList>
+            <div>{temperature9}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>9시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>12시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>15시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>18시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>21시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>24시</div>
+          </WrapList>
+        </Wrap>
+      </Container>
+      {/* 모레 */}
+      <Container>
+        <Title>
+          <h3>모레</h3>
+          <div>
+            <TbTemperature size="20px" />
+            <TiWeatherSunny size="20px" />
+            <TiWeatherShower size="20px" />
+          </div>
+        </Title>{' '}
+        <Wrap>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>0시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>3시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>6시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>9시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>12시</div>
+          </WrapList>
+          <WrapList>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>15시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>18시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>21시</div>
+          </WrapList>
+          <WrapList>
+            <div>{}°C</div>
+            <div>img</div> {/* <img src="" alt="" /> */}
+            <div>24시</div>
+          </WrapList>
+        </Wrap>
+      </Container>
+    </>
+  );
 };
 
 const Container = styled.div`
-	height: 30%;
-	width: 90%;
-	margin: 10px;
-	padding: 15px 5px;
-	background-color: rgb(255 255 255 / 20%);
-	border-radius: 20px;
-	font-size: 14px;
+  height: 60%;
+  width: 85%;
+  margin: 10px;
+  padding: 15px 5px;
+  background-color: rgb(255 255 255 / 20%);
+  border-radius: 20px;
+  font-size: 14px;
 `;
 
 const Wrap = styled.div`
-	display: flex;
-	overflow: hidden;
+  display: flex;
+  overflow: hidden;
 `;
 
 const WrapList = styled.div`
-	margin: 5px 20px;
-	line-height: 30px;
+  margin: 5px 20px;
+  line-height: 30px;
 
-	div {
-		width: 2.2rem;
-	}
+  div {
+    width: 2.2rem;
+  }
 `;
 
 const Title = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
 
-	h3 {
-		font-size: 14px;
-		font-weight: bold;
-		/* padding-right: 200px; */
-	}
+  h3 {
+    font-size: 14px;
+    font-weight: bold;
+    /* padding-right: 200px; */
+  }
 `;
 
 export default WeeklyWeather;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,56 +9,62 @@ import SideBar from '../components/sideBar/SideBar';
 import { registerDragEvent } from '../utils/drag';
 import { inrange } from './../utils/drag';
 
-const Home = ({ weather }) => {
-	const [modalVisible, setModalVisible] = useState(false);
-	const [dragUp, setDragUp] = useState(0);
+const Home = ({ weather, weekly }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [dragUp, setDragUp] = useState(0);
 
-	const openModal = () => {
-		setModalVisible(true);
-	};
+  const openModal = () => {
+    setModalVisible(true);
+  };
 
-	const closeModal = () => {
-		setModalVisible(false);
-	};
+  const closeModal = () => {
+    setModalVisible(false);
+  };
 
-	return (
-		<HomeContainer>
-			<Header location={weather.name} openModal={openModal} />
-			<main
-				// dragUp 이벤트
-				{...registerDragEvent({
-					onDragStart: (moveX, moveY) => {
-						// console.log(moveY);
-						setDragUp(inrange(moveY, -100, 0));
-					},
-					onDragEnd: (moveX, moveY) => {
-						if (moveY <= -100) {
-							setDragUp(1);
-						} else {
-							setDragUp(0);
-						}
-					},
-				})}
-			>
-				<Dresses temperature={Math.round(weather.main.temp)} />
-				<WeatherContainer weather={weather} dragUp={dragUp} />
-			</main>
-			{modalVisible && <SideBar onClose={closeModal} />}
-		</HomeContainer>
-	);
+  return (
+    <HomeContainer>
+      <Header location={weather.name} openModal={openModal} />
+      <main
+        // dragUp 이벤트
+        {...registerDragEvent({
+          onDragStart: (moveX, moveY) => {
+            // console.log(moveY);
+            setDragUp(inrange(moveY, -100, 0));
+          },
+          onDragEnd: (moveX, moveY) => {
+            if (moveY <= -100) {
+              setDragUp(1);
+            } else {
+              setDragUp(0);
+            }
+          },
+        })}
+      >
+        <Dresses temperature={Math.round(weather.main.temp)} />
+        <WeatherContainer weather={weather} dragUp={dragUp} weekly={weekly} />
+      </main>
+      {modalVisible && (
+        <SideBar
+          visible={modalVisible}
+          maskClosable={true}
+          onClose={closeModal}
+        />
+      )}
+    </HomeContainer>
+  );
 };
 
 const HomeContainer = styled.div`
-	position: relative;
-	width: 100%;
-	height: 100%;
-	overflow: hidden;
-	text-align: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  text-align: center;
 
-	main {
-		height: calc(100% - 3rem);
-		margin-top: 3rem;
-	}
+  main {
+    height: calc(100% - 3rem);
+    margin-top: 3rem;
+  }
 `;
 
 export default Home;
