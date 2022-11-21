9863110bd536346c6637b0cca043ba5298b57625
huunguk <huunguk@gmail.com>
2022-11-21 10:41:49 +0900

    style : 코드 오류 수정

--- src/components/sideBar/Location.js
+++ src/components/sideBar/Location.js
@@ -2,13 +2,7 @@
 import React from 'react';
 import LocationList from './LocationList';
 
-<<<<<<< HEAD
-
-const Location = () => { 
-  
-=======
 const Location = () => {
->>>>>>> 01fa693dd915e55b877c89101a1360035280ccf0
   return (
     <div>
       <LocationList />
