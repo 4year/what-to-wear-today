ec5dbe3ec4161b1e19c922da6430df1d437ce486
taeyeess <taeyeeya@gmail.com>
2022-11-22 14:39:53 +0900

    Co-authored-by: Hayoung Park <happai079@users.noreply.github.com> Co-authored-by: Jin Hyeong Uk <huunguk@gmail.com>

--- src/components/addLocation/SearchResult.js
+++ src/components/addLocation/SearchResult.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import LocationList from '../sideBar/LocationList';
 
-const SearchResult = ({ data }) => {
+const SearchResult = ({ cityList }) => {
 	return (
     <div>
-      {data.map((locationList) => (
+      {cityList.map((locationList) => (
         <LocationList key={locationList.id} {...locationList} />
       ))}
     </div>
-    );
+  )
 };
 
 export default SearchResult;
