2918619f342b65f9135b0b7cc2e9f8aa8b5f903e
happai079 <gksyddl22@gmail.com>
2022-12-19 10:06:51 +0900

    fix : loading 페이지 수정

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,5 +1,5 @@
 // 로딩 페이지
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import { API_KEY } from '../config';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -20,60 +20,66 @@ const Loading = () => {
     });
   };
 
-  // 현재 날씨 가져오기
-  const getCurrentWeather = async (lat, lon, name) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
-
-    try {
-      const result = await fetch(url).then(response => response.json());
-
-      // name값이 없으면 id를 이용해 지역이름 -> 한국어로 변경
-      name ??= getCityName(result.id);
-
-      // localStorage에 지역 목록 추가
-      let prevCityList = JSON.parse(localStorage.getItem('CityList')) || [];
-
-      // 이미 있는 지역이면 추가 안 되게
-      prevCityList &&= prevCityList.filter(city => city.name !== name);
-
-      const storeCityList = [
-        ...prevCityList,
-        {
-          name: name,
-          lat: lat,
-          lon: lon,
-          temp: Math.round(result.main.temp),
-          icon: result.weather[0].icon,
-        },
-      ];
-
-      localStorage.setItem('CityList', JSON.stringify(storeCityList));
-
-      // 현재 위치 주간 날씨 가져오기
-      getWeeklyWeather(lat, lon, result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   //주간 날씨 가져오기
-  const getWeeklyWeather = async (lat, lon, result) => {
-    const weeklyUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
+  const getWeeklyWeather = useCallback(
+    async (lat, lon, result) => {
+      const weeklyUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
 
-    try {
-      const weeklyResult = await fetch(weeklyUrl).then(response => response.json());
-      // home으로 이동
-      navigate('/home', {
-        replace: false,
-        state: {
-          result,
-          weeklyResult,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      try {
+        const weeklyResult = await fetch(weeklyUrl).then(response => response.json());
+        // home으로 이동
+        navigate('/home', {
+          replace: false,
+          state: {
+            result,
+            weeklyResult,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [navigate],
+  );
+
+  // 현재 날씨 가져오기
+  const getCurrentWeather = useCallback(
+    async (lat, lon, name) => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
+
+      try {
+        const result = await fetch(url).then(response => response.json());
+
+        // name값이 없으면 id를 이용해 지역이름 -> 한국어로 변경
+        name ??= getCityName(result.id);
+
+        // localStorage에 지역 목록 추가
+        let prevCityList = JSON.parse(localStorage.getItem('CityList')) || [];
+
+        // 이미 있는 지역이면 추가 안 되게
+        prevCityList &&= prevCityList.filter(city => city.name !== name);
+
+        const storeCityList = [
+          ...prevCityList,
+          {
+            name: name,
+            lat: lat,
+            lon: lon,
+            temp: Math.round(result.main.temp),
+            icon: result.weather[0].icon,
+          },
+        ];
+
+        localStorage.setItem('CityList', JSON.stringify(storeCityList));
+
+        // 현재 위치 주간 날씨 가져오기
+        getWeeklyWeather(lat, lon, result);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [getWeeklyWeather],
+  );
 
   // api fetching 후 페이지 이동
   useEffect(() => {
